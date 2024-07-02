59528c020ad4c2bb9b23ff40ba176b00d9cec0f0
temesgen5335 <temesgen5335@gmail.com>
2024-07-02 11:28:45 +0300

    initiating tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} 
+export default {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+*/
+
+// tailwind.config.js
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
