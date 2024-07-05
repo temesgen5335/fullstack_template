1819bfa2351cf121e9892daa6a73e56a14099b95
temesgen5335 <temesgen5335@gmail.com>
2024-07-05 21:03:58 +0300

    navigation bar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav className="bg-blue-600 p-4">
+      <div className="container mx-auto">
+        <h1 className="text-white text-2xl">Contract Advisor Bot</h1>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
