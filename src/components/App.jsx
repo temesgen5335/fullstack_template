34ff07a9ca45b4e7999aa669c91b0e7fb343a930
temesgen5335 <temesgen5335@gmail.com>
2024-07-05 21:06:08 +0300

    app.js

---
+++ src/components/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Navbar from './Navbar';
+import ContractUploader from './ContractUploader';
+import QnADisplay from './QnADisplay';
+
+function App() {
+  return (
+   <div>
+    
+      <div className="flex flex-col min-h-screen bg-gray-100">
+      <Navbar />
+      <div className="flex-grow container mx-auto px-4 py-8">
+        <ContractUploader />
+        <QnADisplay />
+      </div>
+    </div>
+   </div>
+ 
+  );
+}
+
+export default App;
